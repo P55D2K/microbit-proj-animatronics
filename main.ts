a71ab716e0e7fee30d7cef3d3a85175f5afe8f32
zk <124562280+P55D2K@users.noreply.github.com>
2023-09-21 11:02:02 +0800

    s

--- main.ts
+++ main.ts
@@ -9,16 +9,24 @@ input.onButtonPressed(Button.A, function () {
         basic.clearScreen()
         basic.showNumber(time_left_divide_1000)
     }
+    if (time_left_divide_1000 == 0) {
+        pins.analogWritePin(AnalogPin.P1, 1023)
+        pins.analogWritePin(AnalogPin.P2, 1023)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     timer_on = false
     time_left_divide_1000 = 300
     basic.showString("Reset Timer")
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
 })
 input.onButtonPressed(Button.B, function () {
     timer_on = !(timer_on)
     basic.showNumber(time_left_divide_1000)
     basic.showString("Paused timer")
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
 })
 let timer_on = false
 let time_left_divide_1000 = 0
