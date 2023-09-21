1008f12625abe97d75025f05a8f806f852a0ff4f
zk <124562280+P55D2K@users.noreply.github.com>
2023-09-21 11:12:51 +0800

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,27 @@
 input.onButtonPressed(Button.A, function () {
+    pins.analogWritePin(AnalogPin.P1, 0)
     timer_on = true
-    time_left_divide_1000 = 300
+    time_left_divide_1000 = 5
     while (time_left_divide_1000 != 0) {
         if (timer_on) {
             basic.pause(1000)
             time_left_divide_1000 += -1
         }
         basic.clearScreen()
-        basic.showNumber(time_left_divide_1000)
+        basic.showNumber(time_left_divide_1000 + 1)
     }
     if (time_left_divide_1000 == 0) {
         pins.analogWritePin(AnalogPin.P1, 1023)
-        pins.analogWritePin(AnalogPin.P2, 1023)
+        music.play(music.stringPlayable("E - C5 C A - F - ", 120), music.PlaybackMode.UntilDone)
     }
 })
-input.onButtonPressed(Button.AB, function () {
-    timer_on = false
-    time_left_divide_1000 = 300
-    basic.showString("Reset Timer")
-    pins.analogWritePin(AnalogPin.P1, 0)
-    pins.analogWritePin(AnalogPin.P2, 0)
-})
 input.onButtonPressed(Button.B, function () {
-    timer_on = !(timer_on)
-    basic.showNumber(time_left_divide_1000)
-    basic.showString("Paused timer")
+    timer_on = false
+    time_left_divide_1000 = 5
+    basic.showString("r")
     pins.analogWritePin(AnalogPin.P1, 0)
-    pins.analogWritePin(AnalogPin.P2, 0)
 })
 let timer_on = false
 let time_left_divide_1000 = 0
-time_left_divide_1000 = 300
+basic.clearScreen()
+time_left_divide_1000 = 5
