8b89e94b3f11cd9efe28735efc0d6637f4f47ccf
zk <124562280+P55D2K@users.noreply.github.com>
2023-09-21 10:57:57 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
-let timer_on = false
-let time_left_divide_100 = 3000
 input.onButtonPressed(Button.A, function () {
-    if (time_left_divide_100 != 0)
+    timer_on = true
+    time_left_divide_1000 = 300
+    while (time_left_divide_1000 != 0) {
         if (timer_on) {
-            basic.pause(100)
+            basic.pause(1000)
+            time_left_divide_1000 += -1
         }
-        time_left_divide_100--;
+        basic.clearScreen()
+        basic.showNumber(time_left_divide_1000)
     }
 })
 input.onButtonPressed(Button.AB, function () {
     timer_on = false
+    time_left_divide_1000 = 300
+    basic.showString("Reset Timer")
 })
 input.onButtonPressed(Button.B, function () {
-	
+    timer_on = !(timer_on)
+    basic.showNumber(time_left_divide_1000)
+    basic.showString("Paused timer")
 })
+let timer_on = false
+let time_left_divide_1000 = 0
+time_left_divide_1000 = 300
